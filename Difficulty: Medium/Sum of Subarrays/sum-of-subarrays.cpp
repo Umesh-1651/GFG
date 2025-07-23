3166a56699769a771d6cfbb706bf57d988b9400b
Umesh Hemanth Shivaji <122421297+Umesh-1651@users.noreply.github.com>
2025-07-23 22:01:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Subarrays/sum-of-subarrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int subarraySum(vector<int>& arr) {
+     int n=arr.size();
+      int result = 0;
+
+    // computing sum of subarray using formula
+    for (int i = 0; i < n; i++)
+        result += (arr[i] * (i + 1) * (n - i));
+
+    // return all subarray sum
+    return result;
+    }
+};
